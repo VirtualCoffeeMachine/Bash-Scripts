0d953c622f14c683efce2d6b69cf2739728c80d9
Dan Maximuk <danmmaximuk@gmail.com>
2021-08-09 17:57:11 -0500

    mybackup.sh

---
+++ mybackup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Author: Dan M.
+#Created: 08/09/21
+#Last Modified: 08/09/21
+
+#Description:
+# Creates a backup in the current directory of all files in the home directory.
+
+echo "Who is there?"
+read RESPONSE
+echo "Hello there, $RESPONSE"
+echo "I will now back up your home directory, $HOME"
+
+currentdir=$(pwd)
+echo "I will create save the backup file in $currentdir"
+
+tar -cvf $currentdir/my_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar $HOME/* 2>/dev/null
+
+echo "Backup completed successfully"
+
+exit 0
